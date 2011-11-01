39c9428124494163d6741e478ad5290b7ff066ad
Alex Frenkel <sirshurf@yahoo.com>
2011-11-01 09:08:42 -0700

    Added debug option save and load

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -37,12 +37,16 @@ class Labels_IndexController extends Zend_Controller_Action {
 		$grid->registerPlugin ( new Ingot_JQuery_JqGrid_Plugin_ToolbarFilter () );		
 		$this->view->grid = $grid->render (null, array('DblClkEdit'=>TRUE));
 		
+		$objForm = new Labels_Form_UploadBkp();
+		$this->view->objForm = $objForm;
+		
 		// Buttons set
-		$arrButtons = array ();
+		$arrActions = array ();
 		
-//		$arrButtons [] = array ('module' => 'labels', 'controller' => 'index', "action" => "inittable", "name" => 'LBL_BUTTON_MSG_INIT_TABLE' );
+		$arrActions[] = array('module' => 'labels', 'controller' => 'index', "action" => "savebackup", "name" => 'LBL_SAVE_BKP_FILE');
+		$arrActions[] = array('module' => 'labels', 'controller' => 'index', "action" => "loadbackup", "name" => 'LBL_LOAD_BKP_FILE', 'onClick' => '$("#uploader").dialog("open")');
 		
-		$this->view->arrActions = $arrButtons;
+		$this->view->arrActions = $arrActions;
 	}
 	
 	public function editAction() {
@@ -90,7 +94,71 @@ class Labels_IndexController extends Zend_Controller_Action {
 			}
 		}
 		Labels_Model_SystemLabels::initLables(TRUE);
+	}
 	
+
+	public function savebackupAction ()
+	{
+		Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->setNoRender();
+		Zend_Controller_Action_HelperBroker::getStaticHelper('layout')->disableLayout();
+		$objTable = new Labels_Model_Db_SystemLabels();
+		$objSelect = $objTable->select(TRUE);
+		$objConfig = new Zend_Config($objTable->fetchAll($objSelect)->toArray());
+		$writer = new Zend_Config_Writer_Array(array('config' => $objConfig, 'filename' => APPLICATION_PATH . '/configs/backup/labels.php'));
+		try {
+			$writer->write();
+			Labels_Model_SystemLabels::setJgrowlMessage('LBL_MENU_SAVE_OK');
+		} catch (Exception $objEx) {
+			Labels_Model_SystemLabels::setJgrowlMessage('LBL_MENU_SAVE_FAILED');
+		}
+		$strUrl = $this->view->url(array('module' => 'labels', 'controller' => 'index', 'action' => 'index'), null, true);
+		$this->_redirect($strUrl);
+	}
+
+	public function loadbackupAction ()
+	{
+		Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer')->setNoRender();
+		Zend_Controller_Action_HelperBroker::getStaticHelper('layout')->disableLayout();
+		
+		$form = new Menu_Form_UploadMenu();
+		
+		if ($this->_request->isPost()) {
+			$formData = $this->_request->getPost();
+			if ($form->isValid($formData)) {
+				
+				// success - do something with the uploaded file
+				if ($form->menuFile->isUploaded()) {
+					// foo file given... do something
+					$fullFilePath = $form->getValues();
+					$fullFilePath = $form->menuFile->getFileName();
+					Zend_Debug::dump($fullFilePath);
+					
+					$objConfig = new Zend_Config(require $fullFilePath);
+					
+					Zend_Debug::dump($objConfig->toArray());
+					$objMenuTable = new Labels_Model_Db_SystemLabels();
+					
+					foreach ($objConfig as $objConfigMenuRow) {
+						$objMenuRowSet = $objMenuTable->find($objConfigMenuRow->{Labels_Model_Db_SystemLabels::COL_ID_SYSTEM});
+						
+						if ($objMenuRowSet->count() > 0) {
+							$objMenuRow = $objMenuRowSet->current();
+						} else {
+							// Add New
+							$objMenuRow = $objMenuTable->createRow();
+						}
+						$objMenuRow->setFromArray($objConfigMenuRow->toArray());
+						$objMenuRow->save();
+					}
+				}
+			
+			} else {
+				$form->populate($formData);
+			}
+		}
+	
+		$strUrl = $this->view->url(array('module' => 'labels', 'controller' => 'index', 'action' => 'index'), null, true);
+		$this->_redirect($strUrl);
 	}
 	
 	
